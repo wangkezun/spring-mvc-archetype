7b256a8a3278078680a12d259c5ea6c5a243401d
wangkezun <wangkezun@gmail.com>
2015-10-27 00:38:07 +0800

    update

--- src/main/java/com/wangkezun/controller/MainController.java
+++ src/main/java/com/wangkezun/controller/MainController.java
@@ -3,7 +3,11 @@ package com.wangkezun.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by wangkezun on 15/10/26.
@@ -13,9 +17,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class MainController {
     @RequestMapping(value = {"/","/index.htm","/index.html"},method = RequestMethod.GET)
-    public ModelAndView index() {
+    @ResponseBody
+    public Map<String,Object> index() {
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("str","Hello, world!");
-        return mav;
+        Map<String, Object> map = new HashMap<>();
+        map.put("a","Hello, world!");
+        return map;
     }
 }
